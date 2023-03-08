b5f90b72bd5982748b1694d12bc3f214e6717363
kcmair <97917570+kcmair@users.noreply.github.com>
2023-03-07 18:24:24 -0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -12,5 +12,5 @@ const app = express()
 app.use('/', require('./routes'))
 
 app.listen(port,  ()=> {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`MotorMingle app listening on port ${port}`)
 })
