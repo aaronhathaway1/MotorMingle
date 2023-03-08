e8c0aca186dcacbdf26c79de9ee409b6719f35db
Keith Mair <mairck@gmail.com>
2023-03-07 19:01:11 -0700

    tsconfig.json added, server.ts updated

--- server.ts
+++ server.ts
@@ -1,5 +1,8 @@
-const express = require('express')
+import express from 'express'
 const connectMongoose = require('./db/connect')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
 
 require('mongoose')
 require('dotenv').config()
@@ -9,9 +12,21 @@ connectMongoose()
 const port = process.env.PORT || 3000
 const app = express()
 
-console.log(process.env.PORT, port)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+app.use(
+    (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        next()
+    }
+)
+app.use(cors())
 app.use('/', require('./routes'))
-
-app.listen(port,  ()=> {
-    console.log(`Example app listening on port ${port}`)
+app.listen(port, () => {
+    console.log(`MotorMingle app listening on port ${port}`)
 })
