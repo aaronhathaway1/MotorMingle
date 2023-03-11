ccc6cd52ef280bdaeafa91a5aabc56597d4276ef
Keith Mair <mairck@gmail.com>
2023-03-11 14:33:44 -0700

    Added if dev statement for morgan

--- server.ts
+++ server.ts
@@ -2,7 +2,6 @@ import express from 'express'
 import 'dotenv/config'
 const connectMongoose = require('./db/connect')
 const bodyParser = require('body-parser')
-const morgan = require('morgan')
 const cors = require('cors')
 
 require('mongoose')
@@ -15,6 +14,7 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 if (process.env.NODE_ENV === 'development') {
+    const morgan = require('morgan')
     app.use(morgan('dev'))
 }
 app.use(
