6a1141aa6cd18e2936be6f56fc929c55e5d3fdf0
Keith Mair <mairck@gmail.com>
2023-03-11 14:24:17 -0700

    testing

--- build/server.js
+++ build/server.js
@@ -1,29 +1,32 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-require("dotenv/config");
-const connectMongoose = require('./db/connect');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
-require('mongoose');
-connectMongoose();
-const port = process.env.PORT || 3000;
-const app = (0, express_1.default)();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+'use strict'
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod }
+    }
+Object.defineProperty(exports, '__esModule', { value: true })
+const express_1 = __importDefault(require('express'))
+require('dotenv/config')
+const connectMongoose = require('./db/connect')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+require('mongoose')
+connectMongoose()
+const port = process.env.PORT || 3000
+const app = (0, express_1.default)()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+    console.log('if morgan', process.env.NODE_ENV)
+    app.use(morgan('dev'))
 }
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-});
-app.use(cors());
-app.use('/', require('./routes'));
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    next()
+})
+app.use(cors())
+app.use('/', require('./routes'))
 app.listen(port, () => {
-    console.log(`MotorMingle app listening on port ${port}`);
-});
+    console.log(`MotorMingle app listening on port ${port}`)
+})
