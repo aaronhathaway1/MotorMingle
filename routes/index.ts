632e57628f3fd71a3245608b3229c210fceb5108
Keith Mair <mairck@gmail.com>
2023-03-11 11:08:48 -0700

    Swagger documentation working

--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,12 @@
 import Router from 'express'
+import swaggerUi from 'swagger-ui-express'
 
+const swaggerDocument: any = require('../swagger.json')
 const router = Router()
 
-router.use('/clubs', require('./clubs'))
+router
+    .use('/clubs', require('./clubs'))
+    .use('/api-docs', swaggerUi.serve)
+    .get('/api-docs', swaggerUi.setup(swaggerDocument))
 
 module.exports = router
