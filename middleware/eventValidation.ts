2693d29bd0e8c6fa8986b93a84a2c3e1ff6ec895
Keith Mair <mairck@gmail.com>
2023-04-03 21:44:36 -0600

    added if 'POST' to eventValidation.ts

--- middleware/eventValidation.ts
+++ middleware/eventValidation.ts
@@ -1,30 +1,41 @@
-
 import { Request, Response, NextFunction } from 'express'
 
-const validator = require('../helpers/validate');
+const validator = require('../helpers/validate')
 
-const eventValidate = async (req: Request, res: Response, next: NextFunction) => {
-  const validationRule = {
-     
-    date: 'required|string',
-    time: 'required|string',
-    location: 'required|string',
-    organizer: 'required|string',
-  }
-  
-  validator(req.body, validationRule, {}, (err:any, status:any) => {
-    if (!status) {
-      res.status(412).send({
-        success: false,
-        message: 'Validation failed',
-        data: err
-      });
+const eventValidate = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    let validationRule
+
+    if (req.method === 'POST') {
+        validationRule = {
+            date: 'required|string',
+            time: 'required|string',
+            location: 'required|string',
+            organizer: 'required|string',
+        }
     } else {
-      next()
+        validationRule = {
+            date: 'string',
+            time: 'string',
+            location: 'string',
+            organizer: 'string',
+        }
     }
-  })
+
+    validator(req.body, validationRule, {}, (err: any, status: any) => {
+        if (!status) {
+            res.status(412).send({
+                success: false,
+                message: 'Validation failed',
+                data: err,
+            })
+        } else {
+            next()
+        }
+    }).catch((err: any) => console.log(err))
 }
 
-module.exports = {
-  eventValidate
-}
+module.exports = { eventValidate }
