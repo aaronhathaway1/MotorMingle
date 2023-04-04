2ce04d980d2f8d18905335fc10a8ca65f9290743
Aaron Hathaway <46059772+aaronhathaway1@users.noreply.github.com>
2023-04-04 09:23:19 -0600

    Removed ensureAuth temporarily
    This is for testing purposes. authEnsure should be replaced before filming

--- routes/index.ts
+++ routes/index.ts
@@ -7,10 +7,10 @@ const { ensureAuth } = require('../middleware/auth')
 
 router
     .use('/auth', require('./auth'))
-    .use('/person',ensureAuth, require('./person'))
-    .use('/cars', ensureAuth, require('./cars'))
-    .use('/clubs', ensureAuth, require('./clubs'))
-    .use('/events', ensureAuth, require('./events'))
+    .use('/person', require('./person'))
+    .use('/cars',  require('./cars'))
+    .use('/clubs',  require('./clubs'))
+    .use('/events', require('./events'))
     .use('/api-docs', swaggerUi.serve)
     .get('/api-docs', swaggerUi.setup(swaggerDocument))
 
