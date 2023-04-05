55244a5c269a7a126b51e1b139169205dff3c334
Aaron Hathaway <46059772+aaronhathaway1@users.noreply.github.com>
2023-04-04 18:17:31 -0600

    Added ensureAuth
    Added ensureAuth back in

--- routes/index.ts
+++ routes/index.ts
@@ -6,11 +6,11 @@ const router = Router()
 const { ensureAuth } = require('../middleware/auth')
 
 router
-    .use('/auth', require('./auth'))
-    .use('/person', require('./person'))
-    .use('/cars',  require('./cars'))
-    .use('/clubs',  require('./clubs'))
-    .use('/events', require('./events'))
+    .use('/auth', ensureAuth, require('./auth'))
+    .use('/person', ensureAuth, require('./person'))
+    .use('/cars', ensureAuth,  require('./cars'))
+    .use('/clubs', ensureAuth,  require('./clubs'))
+    .use('/events', ensureAuth, require('./events'))
     .use('/api-docs', swaggerUi.serve)
     .get('/api-docs', swaggerUi.setup(swaggerDocument))
 
